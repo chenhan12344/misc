4953aa61e44be282f39eb43b047914f7273791c9
chenhan12344 <chenhan12344@gmail.com>
2019-08-13 10:24:00 +0800

    0813

--- src/main/java/leetcode/easy/LC112.java
+++ src/main/java/leetcode/easy/LC112.java
@@ -1,6 +1,10 @@
 package leetcode.easy;
 
 import structure.tree.TreeNode;
+import structure.tree.TreeUtils;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Sky on 2019/7/3
@@ -16,6 +20,12 @@ import structure.tree.TreeNode;
  */
 public class LC112 {
 
+    public static void main(String[] args) {
+        TreeNode root = TreeUtils.buildBinaryTree(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5);
+        List<String> result = new LC112().hasPathSum(root, 22);
+        System.out.println(String.join("->", result));
+    }
+
     /**
      * 判断树中是否存在路径，使得路径为给定的值
      * 基本思想：
@@ -25,35 +35,6 @@ public class LC112 {
      * @param root 树根
      * @param sum  给定路径和
      */
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) {
-            return false;
-        }
-        int nextSum = sum - root.val;
-        if (nextSum == 0 && root.left == null && root.right == null) {
-            return true;
-        }
-        return hasPathSum(root.left, nextSum) || hasPathSum(root.right, nextSum);
-    }
-
-import structure.tree.TreeUtils;
-
-import java.util.LinkedList;
-import java.util.List;
-
-/**
- * Created by 44399 on 2019/7/4
- *
- * @author 44399
- */
-public class LC112 {
-
-    public static void main(String[] args) {
-        TreeNode root = TreeUtils.buildCompleteBinaryTree(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5);
-        List<String> result = new LC112().hasPathSum(root, 22);
-        System.out.println(String.join("->", result));
-    }
-
     public List<String> hasPathSum(TreeNode root, int sum) {
         if (root == null) {
             return new LinkedList<>();
